19a7d484ee911c455ee7296507f8365109db9f68
alan <ajsantos@rd.com.br>
2021-08-12 17:38:26 -0300

    modelando entidade products

---
+++ src/modules/products/typeorm/entities/Products.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('products')
+class Products {
+    
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column()
+    name: string;
+
+    @Column('decimal')
+    price: number;
+
+    @Column('int')
+    quantity: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default Products;
