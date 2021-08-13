e6a3fd937c8ddace658a820e15d7a1ebbdf65c68
alan <ajsantos@rd.com.br>
2021-08-13 16:38:12 -0300

    respository user

--- src/modules/users/typeorm/entities/User.ts
+++ src/modules/users/typeorm/entities/User.ts
@@ -1,7 +1,7 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('users')
-export default  class USer {
+export default  class User {
 
     @PrimaryGeneratedColumn('uuid')
     id: string;
