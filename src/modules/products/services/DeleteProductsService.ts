769bf87d46438d9c3d6fb95d298d535310c7f7fe
alan <ajsantos@rd.com.br>
2021-08-13 11:34:40 -0300

    servico para remover produto

---
+++ src/modules/products/services/DeleteProductsService.ts
@@ -0,0 +1,30 @@
+import AppError from "@shared/errors/AppError";
+import { getCustomRepository } from "typeorm";
+import Products from "../typeorm/entities/Products";
+import { ProductRepository } from "../typeorm/repositories/ProductsRepository";
+
+interface IRequest {
+    id: string
+}
+
+class DeleteProductsService {
+
+    //metodo para criar um produto
+    public async excetute({id}: IRequest): Promise<void> {
+
+        const productsRepo = getCustomRepository(ProductRepository);
+  
+        //buscando todos os produtos
+        const products = await productsRepo.findOne(id)
+        
+        if (!products) {
+            throw new AppError('Produto não existe!')
+        }
+
+        //deletando produto
+        await productsRepo.remove(products);
+
+    }
+}
+
+export default DeleteProductsService;
