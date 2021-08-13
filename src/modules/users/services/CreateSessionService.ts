e13d9a518a753f286d03e15a99e5c7ad817d04a1
alan <ajsantos@rd.com.br>
2021-08-13 18:26:05 -0300

    servico para criacao de session do usuario par ao token

---
+++ src/modules/users/services/CreateSessionService.ts
@@ -0,0 +1,37 @@
+import AppError from "@shared/errors/AppError";
+import { compare, hash } from "bcryptjs";
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import UserRepository from "../typeorm/repositories/UserRepository";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+export default class CreaSessionService {
+
+    //metodo para criar um token
+    public async createUser({email, password}: IRequest): Promise<User> {
+        const userRepo = getCustomRepository(UserRepository);
+        const user = await userRepo.findByEmail(email);
+
+        //testando se existe usuario com este email no banco de dados.
+        if (!user) {
+            throw new AppError('Incorret email/password combination', 401);
+        }
+
+        //comprando a senha criptografada com a senha do usuario
+        const passwordConfirmed = await compare(password, user.password);
+
+        if (!passwordConfirmed) {
+            throw new AppError('Incorret email/password combination');   
+        }
+        //salvando o objeto
+        await userRepo.save(user);
+
+        //retornando objeto salvo
+        return user;
+    
+    }
+}
