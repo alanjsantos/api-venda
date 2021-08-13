c3d48c4e061eb86d72c07abddfc89000563b89b6
alan <ajsantos@rd.com.br>
2021-08-13 15:41:25 -0300

    validacao dos dados na requisicao

--- src/modules/products/routes/ProductsRoutes.ts
+++ src/modules/products/routes/ProductsRoutes.ts
@@ -1,13 +1,54 @@
 import { Router } from "express";
 import ProductsController from "../controller/ProductsController";
+import { celebrate, Joi, errors, Segments } from "celebrate";
 
 const productesRoutes = Router();
 const productsController = new ProductsController();
 
+//endpoint listar products
 productesRoutes.get('/', productsController.listProduct);
-productesRoutes.get('/:id', productsController.showProducts);
-productesRoutes.post('/', productsController.createProducts);
-productesRoutes.put('/:id', productsController.updateProducts)
-productesRoutes.delete('/:id', productsController.deleteProducts);
+
+//endpoint buscar por products por id
+productesRoutes.get('/:id',
+ celebrate({
+     [Segments.PARAMS]: {
+         id: Joi.string().uuid().required(),
+             }
+           }),  
+productsController.showProducts);
+
+//endpoint criar products
+productesRoutes.post('/',
+ celebrate({
+     [Segments.BODY]: {
+         name: Joi.string().required(),
+         price: Joi.number().precision(2).required(),
+         quantity: Joi.number().required()
+     }
+ }),
+productsController.createProducts);
+
+//endpoint atualizar products
+productesRoutes.put('/:id', 
+celebrate({
+    [Segments.BODY]: {
+        name: Joi.string().required(),
+        price: Joi.number().precision(2).required(),
+        quantity: Joi.number().required()
+    },
+    [Segments.PARAMS]: {
+        id: Joi.string().uuid().required(),
+            }
+}),
+productsController.updateProducts)
+
+//endpoint deletar products.
+productesRoutes.delete('/:id',
+celebrate({
+    [Segments.PARAMS]: {
+        id: Joi.string().uuid().required(),
+            }
+          }),  
+productsController.deleteProducts);
 
 export default productesRoutes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import 'reflect-metadata';
 import express, { NextFunction, Request, response, Response } from 'express'
 import 'express-async-errors';
 import cors from 'cors'
+import { errors } from "celebrate";
 import routes from './routes' 
 import '@shared/typeorm'
 import AppError from '@shared/errors/AppError';
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors())
 
 //tratando exception.
 app.use((error: Error, request: Request, resposne: Response, next: NextFunction) => {
