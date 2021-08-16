192acfbe78619c999c1ab709134bacbff1c05c44
alan <ajsantos@rd.com.br>
2021-08-16 14:19:32 -0300

    configuracao da lib para Uploads

---
+++ src/config/upload.ts
@@ -0,0 +1,19 @@
+import path from "path"
+import multer from "multer";
+import crypto from 'crypto'
+
+const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+
+export default {
+    director: uploadFolder;
+    storage: multer.diskStorage({
+        destination: uploadFolder,
+        filename(request, file, callBack) {
+            const fileHash = crypto.randomBytes(10).toString('hex');
+
+            const filename = `${fileHash}-${file.originalname}`;
+            
+            callBack(null, filename)
+        }
+    })
+}
--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,7 +3,7 @@ import AppError from "@shared/errors/AppError";
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
 
-interface TokenPayload {
+interface ITokenPayload {
     iat: number;
     exp: number;
     sub: string;
@@ -21,14 +21,14 @@ export default function isAuthenticated(resquest: Request, response: Response, n
      try {
          const decodedToken = verify(token, auth.jwt.secret)
          
-         const {sub} = decodedToken as TokenPayload;
+         const {sub} = decodedToken as ITokenPayload;
          
          resquest.user = {
              id: sub
          }
 
          return next();
-         
+
      } catch {
          throw new AppError('Invalid JWT token');
          
