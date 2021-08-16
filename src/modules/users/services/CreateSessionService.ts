536d88fa804c0fb7c77d03dcf1ee97961e6b55fa
alan <ajsantos@rd.com.br>
2021-08-16 10:23:58 -0300

    gerando token uer

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -1,5 +1,6 @@
 import AppError from "@shared/errors/AppError";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import UserRepository from "../typeorm/repositories/UserRepository";
@@ -8,14 +9,16 @@ interface IRequest {
     email: string;
     password: string;
 }
-// interface IResponse {
-//     user: User
-// }
+
+interface IResponse {
+    user: User
+    token: string;
+}
 
 export default class CreaSessionService {
 
     //metodo para criar um token
-    public async userAuthentication({email, password}: IRequest): Promise<User> {
+    public async userAuthentication({email, password}: IRequest): Promise<IResponse> {
         const userRepo = getCustomRepository(UserRepository);
         const user = await userRepo.findByEmail(email);
 
@@ -29,10 +32,18 @@ export default class CreaSessionService {
 
         if (!passwordConfirmed) {
             throw new AppError('Incorret email/password combination', 401);   
-        }
+        }   
+
+        const token = sign({}, '639698693822182ed3a9dc179c0c9618', {
+            subject: user.id,
+            expiresIn: '1d'
+        })
 
         //retornando objeto salvo
-        return user;
+        return {
+            user,
+            token
+        };
     
     }
 }
