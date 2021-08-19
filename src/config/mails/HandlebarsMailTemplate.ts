4d70d62cbee24357c9fb6e92f40751c74b0b58af
alan <ajsantos@rd.com.br>
2021-08-19 11:49:50 -0300

    mail file template com handlers

--- src/config/mails/HandlebarsMailTemplate.ts
+++ src/config/mails/HandlebarsMailTemplate.ts
@@ -1,19 +1,22 @@
 import handlebars from 'handlebars';
+import fs from 'fs'
 
 interface ITemplateVariable {
     [key: string]: string | number;
 }
 
 interface IParseMailTemplate {
-    template: string;
+    file: string;
     variables: ITemplateVariable;
 }
 
 export default class HandlebarsMailTemplate {
 
-    public async parse({template, variables}: IParseMailTemplate): Promise<string>  {
-        const parseTemaplate = handlebars.compile(template)
+    public async parse({file, variables}: IParseMailTemplate): Promise<string>  {
 
+        const tempateFileContent = await fs.promises.readFile(file, {encoding: 'utf-8'})
+        
+        const parseTemaplate = handlebars.compile(tempateFileContent)
         return parseTemaplate(variables);
     }
 }
