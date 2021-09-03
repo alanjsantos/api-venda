760eb8ac1d5decdc9452e72a6eb5be59169dcd10
santos <alanjosedossantos011@gmail.com>
2021-09-03 09:39:43 -0300

    refatorando a camada de repository customer

---
+++ src/modules/customers/domain/repositories/ICustomersRepository.ts
@@ -0,0 +1,10 @@
+import { ICreatedCustomer } from "../models/ICreatedCustomer";
+import { ICustomer } from "../models/ICustomer";
+
+export interface ICustomersRepository {
+    findByName(name: string): Promise<ICustomer | undefined>
+    findById(id: string): Promise<ICustomer | undefined>
+    findByEmail(email: string): Promise<ICustomer | undefined>
+    // create(data: ICreatedCustomer): Promise<ICustomer>;
+    // save(customer: ICustomer): Promise<ICustomer>;
+}
--- src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
+++ src/modules/customers/infra/typeorm/repositories/CustomersRepository.ts
@@ -1,8 +1,9 @@
+import { ICustomersRepository } from "@modules/customers/domain/repositories/ICustomersRepository";
 import { EntityRepository, Repository } from "typeorm";
 import Customers from "../entities/Customers";
 
 @EntityRepository(Customers)
-export default class CustomersRepository extends Repository<Customers> {
+export default class CustomersRepository extends Repository<Customers>  implements ICustomersRepository{
 
     public async findByName(name: string): Promise<Customers | undefined>{
         const customers = await this.findOne({
